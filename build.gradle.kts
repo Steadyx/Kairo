ab4de27b660bfc239a73b36befab96f0f6159de8
Edward Kemp <edwardlanekemp@gmail.com>
2025-12-20 20:09:08 +0000

    Add detekt support

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,20 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+    pluginManager.withPlugin("org.jetbrains.kotlin.android") {
+        apply(plugin = "dev.detekt")
+    }
+    pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+        apply(plugin = "dev.detekt")
+    }
+}
+
+tasks.matching { it.name == "detekt" }.configureEach {
+    dependsOn(":app:detektMain")
+    // optionally:
+    // dependsOn(":app:detektTest")
 }
