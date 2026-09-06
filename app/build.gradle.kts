2e5effaa794332aebb80c1d8b24e3269aa525317
Edward Kemp <edwardlanekemp@gmail.com>
2026-09-06 14:44:04 +0100

    chore(release): bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ android {
         applicationId = "com.kairo.reader"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 14
-        versionName = "1.2.0"
+        versionCode = 15
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
