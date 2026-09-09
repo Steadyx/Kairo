535750ee8ee33a590e4c259650ea698b959e7642
Edward Kemp <edwardlanekemp@gmail.com>
2026-09-09 13:54:22 +0100

    test(rsvp): cover context modes and word separation on device

--- app/src/debug/java/com/kairo/reader/TestActivity.kt
+++ app/src/debug/java/com/kairo/reader/TestActivity.kt
@@ -1,5 +1,12 @@
 package com.kairo.reader
 
+import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 
-class TestActivity : ComponentActivity()
+class TestActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+}
